555eea947cbe8b5aa4e52fc226b99c30a43b127c
baltazargch <baltazargch@gmail.com>
2025-09-16 17:14:38 -0700

    add minor scripting to phenology. Possible will delete

--- R/00_prepare_taxon_id_phenology.R
+++ R/00_prepare_taxon_id_phenology.R
@@ -4,10 +4,14 @@ plants <- read_csv('outputs/records/all_species_records_and_native.csv') %>%
   rename_with(janitor::make_clean_names) %>% 
   filter(ca_native == 'yes')
 
-
+plants_clean <- plants %>% 
+  mutate(species = case_when(
+    species == 'Mahonia aquifolium' ~ 'Berberis aquifolium', 
+    .default = species))
 
 cch2_taxonid <- read_csv('inputs/plants_phenology/Native Plants of California_1758044999.csv')
 
-cch2_in <- cch2_taxonid %>% filter(ScientificName  %in% plants$species)
+cch2_in <- cch2_taxonid %>% filter(ScientificName  %in% plants_clean$species)
 
-plants$species[!plants$species  %in% cch2_in$ScientificName]
+plants_clean$species[!plants_clean$species  %in% cch2_taxonid$ScientificName] %>% 
+  paste(., collapse = ', ')
